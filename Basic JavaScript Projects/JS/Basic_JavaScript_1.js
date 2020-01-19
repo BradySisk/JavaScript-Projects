14dc81ef35b5bdceb6d5ca9341f870eee544f7a0
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-01-19 11:05:17 -0600

    js and basic html

--- Basic JavaScript Projects/JS/Basic_JavaScript_1.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -3,7 +3,16 @@ document.write("Hello,World");
 
 var  Family = "Sisks", Dad="jr", Mom= "Charlotte";
 var Family= Family.fontcolor("orange");
+var Dad= Dad.fontcolor("yellow");
+var Mom= Mom.fontcolor("pink");
 document.write(Family);
+document.write(Dad);
+document.write(Mom);
+
+document.write(8+8);
 
 
-Document.write(1+1);
+
+
+
+
