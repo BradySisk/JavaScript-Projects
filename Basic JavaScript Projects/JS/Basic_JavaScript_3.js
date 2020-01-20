b4daa28c9adf967aeed87ad1472fe2b7a84faf69
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-01-19 19:22:49 -0600

    javascript

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,58 @@
+function multiplication() {
+    var simple_math = 6*8;
+    document.getElementById("math").innerHTML = "6*8 = " +simple_math;
+}
+
+var v = 5.25;
+x--;
+document.write(v);
+
+var x = 5;
+x++;
+document.write(x);
+
+window.alert(Math.random() * 100);
+
+function myFunction() {
+    var addition = 2+2;
+    document.getElementById("Math").innerHTML = "2 + 2 =" + addition
+}
+    function myFunction1() {
+        var subtraction = 3 - 2;
+        document.getElementById("Math1").innerHTML = "3 - 2 =" + subtraction
+    }
+
+    function myFunction2() {
+        var multiplication = 3 * 2;
+        document.getElementById("Math2").innerHTML = "3 * 2 =" + multiplication
+    }
+
+    function myFunction3() {
+        var division = 3 / 2;
+        document.getElementById("Math3").innerHTML = "3 / 2 =" + division
+    }
+
+    function myFunction4() {
+        var simple_Math = 10 % 7;
+        document.getElementById("Math4").innerHTML= "when you divide 10 by 7 you have a remainder of:" +simple_Math;
+    }
+
+    function myFunction5() {
+        var x = 7;
+        document.getElementById("Math5").innerHTML = -x
+    }
+
+
+
+function my_Dictionary () {
+    var Animal ={
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!"
+    };
+    document.getElementById("Dictionary").innerHTML = Animal.Sound;
+    }
+
+    document.write(Math.round(7.7));
