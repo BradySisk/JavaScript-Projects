638b57d87d13c8139ed18cf2e54a19e5a85e6695
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-01-22 21:43:45 -0600

    update to js and html 7

--- Basic JavaScript Projects/JS/Basic_JavaScript_7.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_7.js
@@ -16,6 +16,32 @@ Add_numbers1();
      }
  }
  
- 
+ function Age_Function() {
+    Age= document.getElementById("Age").value;
+    if (Age >= 21) {
+        Drink = "You are old enough to party!!!!";
+    }
+    else {
+        Drink = "You are not old enough to do party :(";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Drink;
+ }
+
+
+
+ function Time_function() {
+     var Time = new Date().getHours();
+     var Reply;
+     if (Time < 12 == Time > 0) {
+         Reply = "It is morning time!";
+     }
+     else if (Time > 12 == Time < 18) {
+         Reply = "It is the afternoon.";
+     }
+     else {
+         Reply = "It is evening time.";
+     }
+     document.getElementById("Time_of_day").innerHTMl = Reply;
+ }
  
 
