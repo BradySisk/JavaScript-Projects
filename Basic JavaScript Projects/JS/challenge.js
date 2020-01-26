ab253e662b2dca40ed0cab378912a4140a4f018d
MarkWahlbang <52722077+MarkWahlbang@users.noreply.github.com>
2020-01-25 21:30:44 -0600

    challenge_valueof

---
+++ Basic JavaScript Projects/JS/challenge.js
@@ -0,0 +1,11 @@
+function myFunction1() {
+var num= 6.5484;
+var n = num.toFixed();
+document.getElementById("numFix").innnerHTML = n;
+}
+
+function myFunction () {
+    var str ="Where did he come from,where did he go?";
+    var res =str.valueOf();
+    document.getElementById("cotton").innerHTML =res;
+}
